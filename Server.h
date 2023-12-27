4682fc45d5b904f88845a84f80c072fe18857477
太陽 岡山 <220069@jc-21.jp>
2023-12-27 18:35:36 +0900

    クラス図にあった関数作成：機能はまだない。

--- Server.h
+++ Server.h
@@ -1,1 +1,12 @@
 #pragma once
+
+class Server
+{
+private:
+
+public:
+	void GetPlayerData();
+	void GetEnemyData();
+	void SetPlayerData();
+	void SetEnemyData();
+};
--- Socket.h
+++ Socket.h
@@ -1,5 +1,16 @@
 #pragma once
+
 class Socket
 {
+private:
+
+public:
+	void WSAStartup();
+	void SocketCreate();
+	void Bind();
+	void Send();
+	void Recv();
+	void CloseSocket();
+	void WSACleanup();
 };
 
